8eb366748c2776b19ea73625211efca339a994ea
Ezequiel Farioli <efarioli@gmail.com>
2017-12-31 18:57:01 +0000

    config mongoose data schema

--- app.js
+++ app.js
@@ -3,13 +3,25 @@ var bodyParser = require("body-parser"),
     express    = require("express"),
     app        = express();
 
-mongoose.connect("mongodb://localhost/restfull_blog_app");
+//APP CONFIG
+    mongoose.connect("mongodb://localhost/restfull_blog_app");
 app.set("view engine", "ejs");
 app.use(express.static("public"));
 app.use(bodyParser.urlencoded({extended: true}));
 
+//MONGOOSE /MODEL CONFIG
+var blogSchema = new mongoose.Schema({
+    title: String,
+    image: String,
+    body: String,
+    created: {type: Date, default: Date.now}
+});
+var Blog = mongoose.model("Blog", blogSchema);
+
+//RESTFUL ROUTES
 
 
 app.listen(3000, '0.0.0.0', function(){
     console.log("SERVER IS RUNNIG");
 });
+
