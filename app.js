891e00b632b35e289695d6f36ea78a9cffbbe965
Ezequiel Farioli <efarioli@gmail.com>
2018-01-06 13:47:04 +0000

    add Edit Route

--- app.js
+++ app.js
@@ -70,7 +70,13 @@ app.get("/blogs/:id", function(req, res){
 
 //EDIT ROUTE
 app.get("/blogs/:id/edit", function(req, res){
-    res.render("edit");
+    Blog.findById(req.params.id, function(err, foundBlog){
+        if(err){
+            res.redirect("/blogs");
+        } else {
+            res.render("edit", {blog: foundBlog});
+        }
+    });
 });
 
 
